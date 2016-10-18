d4c6c3dc54c42e8c9a907c8e4d381725417ba5a3
Jin Choi <jinchoi721@gmail.com>
2016-10-18 10:57:05 -0500

    update

---
+++ htseq_to_count.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+#python htseq_to_count.py gtf *count
+#python htseq_to_count.py inter.gtf /Users/jinchoi/brown_count/*bam.count.txt > count.txt
+import sys
+gtf = []
+prev = ""
+for line in open(sys.argv[1],'r'):
+    spl = line.strip().split('\t')
+    if (spl[2] == "exon"):
+        ids = spl[8].split('"')[1]
+        le = int(spl[4])-int(spl[3])
+        gtf.append([ids,str(le)])
+
+count = {}
+files = ["gene","length","description"]
+for file in sys.argv[2:]:
+    files.append(file)
+    temp = []
+    for line in open(file,'r'):
+        if(line[:2] != "__"):
+            spl = line.strip().split('\t')
+            if(count.has_key(spl[0])):
+                temp = count[spl[0]]
+                temp.append(spl[1])
+                count[spl[0]] = temp
+            else:
+                temp = [spl[1]]
+                count[spl[0]] = temp
+
+print '\t'.join(files)
+    
+for x in gtf:
+    final = [x[0],x[1],x[0]]
+    for y in count[x[0]]:
+        final.append(y)
+    print '\t'.join(final)
+
