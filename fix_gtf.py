2be226c39cd74e0b76cc42d0015e22a79f2eb81e
Jin Choi <jinchoi721@gmail.com>
2016-10-24 15:05:54 -0500

    update

--- fix_gtf.py
+++ fix_gtf.py
@@ -5,8 +5,10 @@ for line in open(sys.argv[1],'r'):
     if(line[:1] == "#"):
         continue
     spl = line.strip().split('\t')
-    result = [spl[0],spl[1],"exon"]
+    ids = spl[0].split('_')
+    fid = '_'.join(ids[:-3])
+    result = [fid,spl[1],"exon"]
     for i in range(3,len(spl)):
         result.append(spl[i])
     print '\t'.join(result)
-    print line.strip()
+
